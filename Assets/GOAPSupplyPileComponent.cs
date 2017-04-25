a3d39d8992edebfdf52643ae80b4d1e7c2b771f3
CohenLee <cohenlee@163.com>
2017-04-25 14:10:04 +0800

    阿三

---
+++ Assets/GOAPSupplyPileComponent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GOAPSupplyPileComponent : MonoBehaviour {
+
+	// Use this for initialization
+    public int numTools; // 工具的数量
+    public int numLogs; //  原木的数量，樵夫砍过的树木
+    public int numFirewood; //  木材，伐木工制造的木材数量
+    public int numOre; // 矿石的数量
+}
---
+++ Assets/Scripts/GOAPToolComponent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GOAPToolComponent : MonoBehaviour {
+
+    public float Strength;
+	void Start () {
+        Strength = 1;
+	}
+    //使用工具
+    public void use(float damage) 
+    {
+        Strength -= damage;
+    }
+    //销毁工具
+    public bool destroyed() 
+    {
+        return Strength <= 0;
+    }
+}
